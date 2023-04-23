3e2242275dc7ecafc29b2264362b0c201e0e6ad6
JFmaia <jfmaia741@gmail.com>
2023-04-23 13:42:43 -0300

    Using Docheck and your cicle of life

--- my-primary-project/src/app/app.component.ts
+++ my-primary-project/src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
     <app-title title="Olá Mundo"></app-title>
+    <hr>
+    {{valor}}
+    <button (click)="adicionar()">Adicionar</button>
     <router-outlet></router-outlet>
   `
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit,DoCheck, AfterContentChecked, AfterContentInit,AfterViewInit,AfterViewChecked {
+  public valor:number = 1;
   constructor(){
 
+  }
+
+  public adicionar(): number{
+    return this.valor +=1;
   }
 
   //sempre quando o component inicia é executado
@@ -17,5 +25,25 @@ export class AppComponent implements OnInit {
    setTimeout(() =>{
     console.log(1);
    },5000)
+  }
+
+  ngDoCheck(): void {
+    console.log("DoCheck")
+  }
+
+  ngAfterContentInit(): void {
+    console.log("AfterContentInit")
+  }
+
+  ngAfterContentChecked(): void {
+    console.log("AfterContentChecked")
+  }
+
+  ngAfterViewInit(): void {
+    console.log("AfterViewInit")
+  }
+
+  ngAfterViewChecked(): void {
+    console.log("AfterViewChecked")
   }
 }
