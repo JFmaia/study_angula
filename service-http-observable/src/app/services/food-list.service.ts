ca2de1ab2c1fe91532f912801f92c723b2829555
JFmaia <jfmaia741@gmail.com>
2023-06-02 09:44:22 -0300

    feat: Adicionando modulo e primeiro component

---
+++ service-http-observable/src/app/services/food-list.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FoodListService {
+  //Variaveis
+  private list: Array<string> = [
+    "X bacon",
+    "X galinha",
+    "D-queles"
+  ];
+
+
+  constructor() { }
+
+  public getFoodList(){
+    return this.list;
+  }
+}
