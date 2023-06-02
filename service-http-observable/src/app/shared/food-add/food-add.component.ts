e93ec665720171256bc82ac6aff68582dc92bb8f
JFmaia <jfmaia741@gmail.com>
2023-06-02 11:22:50 -0300

    feat: Adicionando componet de add item

---
+++ service-http-observable/src/app/shared/food-add/food-add.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { FoodListService } from 'src/app/services/food-list.service';
+
+@Component({
+  selector: 'app-food-add',
+  templateUrl: './food-add.component.html',
+  styleUrls: ['./food-add.component.css']
+})
+export class FoodAddComponent implements OnInit{
+  constructor(private foodListService:FoodListService){}
+
+  public listAddItem(value: string){
+    return this.foodListService.foodListAdd(value);
+  }
+
+  ngOnInit(): void {
+
+  }
+}
