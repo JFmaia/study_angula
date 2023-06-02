1fcf7a15b77d63dbcc21b18e09313d6f13e6cdb0
JFmaia <jfmaia741@gmail.com>
2023-06-02 08:35:04 -0300

    feat: Novo modulo

--- service-http-observable/src/app/app.component.ts
+++ service-http-observable/src/app/app.component.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <app-menu></app-menu>
-    <router-outlet></router-outlet>
-  `,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title = 'service-http-observable';
 }
